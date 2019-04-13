d4520fcf245ea0a60a6d82f21ac6738eb2da7e32
ninga123 <44343634+ninga123@users.noreply.github.com>
2019-04-13 20:48:18 +0300

    Add files via upload

---
+++ Navigation.js
@@ -0,0 +1,25 @@
+
+ 
+ window.addEventListener("load",Start,false)
+ 
+ function Start() {
+    	//hide all sections
+    ShowSection("Welcome")
+    
+ }
+ function ShowSection(name){
+    var Welcome = document.getElementById('Welcome');
+    Welcome.style.visibility="hidden";
+    var Register = document.getElementById('Register');
+    Register.style.visibility="hidden";
+    var Login = document.getElementById('Login');
+    Login.style.visibility="hidden";
+    var Configuration = document.getElementById('Configuration');
+    Configuration.style.visibility="hidden";
+    var About = document.getElementById('About');
+    About.style.visibility="hidden";	
+    var Game = document.getElementById('Game');
+    Game.style.visibility="hidden";
+    var selected = document.getElementById(name);
+    	selected.style.visibility="visible";
+ }
